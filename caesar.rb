55da674fb6b1e43b9e7d04a33947543b15de5b0a
gary boland <garyboland@gmail.com>
2016-08-21 23:18:53 +0100

    caesar.rb

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,23 @@
-def caesar(string, shift)
+require 'pry'
+class Caesar
+def shift(string, shift)
+  res=[]
   alphabet = ('a'..'z').to_a
-  nums = (0..25).to_a
+  nums = (1..26).to_a
   hash = Hash[alphabet.zip(nums)]
+  rhash = Hash[nums.zip(alphabet)]
+  string.each_char do |x|
+    if x == ' ' 
+      res.push ' '
+    else
+      score = ((hash[x] + shift) % 26)
+      res.push rhash[score]
+    end
+  end
+  puts res.join('')
+end
+end
 
+
+c=Caesar.new
+c.shift('what a string', 5)
