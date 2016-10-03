762f87132f7ccfc44b0a2bff37f5fa4b94dded6b
Gary Boland <gary.boland@workday.com>
2016-10-03 13:07:00 +0100

    codefightreg.rb
    Change-Id: I0467ffd6ed45a60801196acc7bac2730f1c85b7f

---
+++ codefightreg.rb
@@ -0,0 +1,18 @@
+def bugsAndBugfixes(rules)
+  matches = []
+  pat = /(?<rolls>\d+)?d(?<sides>\d+)(?<sign>\+|\-)?(?<mod>\d+)?/
+  rules.scan(pat) { matches << $~ }
+  result = 0
+  matches.each do |match|
+    c = Hash[ match.names.zip( match.captures ) ]
+    c['rolls'] = 1 unless c['rolls'] 
+    if c['sign'] == '-'
+      tmp=(c['rolls'].to_i * c['sides'].to_i) - c['mod'].to_i
+    else
+      tmp=(c['rolls'].to_i * c['sides'].to_i) + c['mod'].to_i
+    end
+    p "#{tmp} #{c}"
+    result += tmp
+  end
+  result
+end
