063dae2d42fff7dcd11170d72e255c304a081ae1
gary boland <garyboland@gmail.com>
2016-10-08 22:55:49 +0100

    call.rb

---
+++ call.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+def phoneCall(min1, min2_10, min11, s)
+  result = 0
+  while s > 0
+    if result == 0
+      if s - min1 >= 0
+        s -= min1
+        result += 1
+      else
+        break
+      end
+    elsif result.between?(1, 10)
+       if s - min2_10 >= 0
+         s -= min2_10
+         result += 1
+      else
+        break
+       end
+    else
+      if s - min11 >= 0
+        s -= min11
+          result += 1
+      else
+        break
+      end
+    end
+    p "result #{result} s: #{s}"
+    result
+  end
+end
+
+phoneCall(3, 1, 2, 20)
+
