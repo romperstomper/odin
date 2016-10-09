a88c4dbf2966ce61eb2ba0ba43e3396cf287b301
gary boland <garyboland@gmail.com>
2016-10-09 11:23:36 +0100

    call.rb

--- call.rb
+++ call.rb
@@ -3,6 +3,7 @@
 def phoneCall(min1, min2_10, min11, s)
   result = 0
   while s > 0
+    p "before result: #{result} s: #{s}"
     if result == 0
       if s - min1 >= 0
         s -= min1
@@ -10,21 +11,25 @@ def phoneCall(min1, min2_10, min11, s)
       else
         break
       end
-    elsif result.between?(1, 10)
+    elsif result.between?(1, 9)
        if s - min2_10 >= 0
          s -= min2_10
          result += 1
       else
         break
        end
-    else
+    elsif result >=10
       if s - min11 >= 0
         s -= min11
         result += 1
       else
         break
       end
+    else
+      break
     end
-    result
+    p "after result: #{result} s: #{s}"
   end
+  result
 end
+p phoneCall(10, 1, 2, 22)
