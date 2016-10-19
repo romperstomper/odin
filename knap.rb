ac074a9df57eb5ae0a4a7d407efa5e49a300c6a9
gary boland <garyboland@gmail.com>
2016-10-19 21:58:45 +0100

    knap.rb

--- knap.rb
+++ knap.rb
@@ -3,14 +3,14 @@
 def knapsackLight(value1, weight1, value2, weight2, maxW)
   capacity_left = maxW
   result = 0
-  h = Hash[weight1, value1, weight2, value2]
-  h.keys.each do |x|
-    if capacity_left -x >= 0
-      result += h[x]
+  h = Hash[value1, weight1, value2, weight2]
+  h.keys.sort_by {|k,v| k}.reverse.each do |k|
+    if capacity_left -h[k] >= 0
+      result += k
+      capacity_left -= h[k]
     end
   end
   result
 end
 
-p knapsackLight(10, 5, 6, 4, 9)
-
+p knapsackLight(10, 5, 6, 4, 8)
