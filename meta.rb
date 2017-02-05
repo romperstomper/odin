9a3efe3cab1fef4c9070ddd2266561dc54fc9f60
gary boland <garyboland@gmail.com>
2017-02-05 13:17:32 +0000

    magicwell.rb

---
+++ meta.rb
@@ -0,0 +1,23 @@
+class Array
+  def foldl(method)
+    inject { |result, i| result ? result.send(method, i) : i } 
+  end
+end
+
+#puts [1000.0, 200.0, 50.0].foldl("/")
+
+class String
+  def split
+    res = []
+    chars.each do |x|
+      p x
+    end
+  end
+end
+
+
+
+
+
+
+
